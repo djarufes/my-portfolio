f59939d293afa2c15f8019fdb72fcfc2ade52b44
djarufes <djarufes@users.noreply.github.com>
2021-06-26 04:45:07 +0000

    Prettified Code!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 
-const routes: Routes = [
-  { path: '', component: MainComponent}
-];
+const routes: Routes = [{ path: '', component: MainComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
